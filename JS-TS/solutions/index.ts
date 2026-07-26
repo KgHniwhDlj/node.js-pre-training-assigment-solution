4ac8ffba1cd27cc036b6859ce470ce96a0d399eb
KgHniwhDlj <sasafomina03@gmail.com>
2026-07-26 20:16:03 +0300

    Complete js-ts task 10

--- JS-TS/solutions/index.ts
+++ JS-TS/solutions/index.ts
@@ -1,3 +1,45 @@
 #!/usr/bin/env ts-node
 // CLI entry for Task 10 – placeholder only
-console.log('CLI not implemented yet');
+//console.log('CLI not implemented yet');
+import {ToDoManager} from "./todo-manager";
+
+async function main() {
+    try {
+        const manager = new ToDoManager();
+
+        await manager.init();
+
+        const command = process.argv[0];
+        const arg1 = process.argv[1];
+        const arg2 = process.argv[2];
+
+        switch (command) {
+            case 'add':
+                if (!arg1) {
+                    console.error('Please, enter title');
+                    break;
+                }
+                await manager.add(arg1, arg2);
+                console.log(`Todo "${arg1}" has been added!`);
+                break;
+
+            case 'complete':
+                if (!arg1) {
+                    console.error('Please, enter ID');
+                    break;
+                }
+                await manager.complete(Number(arg1));
+                console.log(`The status of the todo ${arg1} has been changed to COMPLETE!`);
+                break;
+
+            case 'list':
+            default:
+                const todos = await manager.list();
+                console.log('All Todos:');
+                console.log(todos);
+                break;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
--- JS-TS/solutions/todo-manager.ts
+++ JS-TS/solutions/todo-manager.ts
@@ -6,18 +6,28 @@ export class ToDoManager {
   private service = new TodoService(new TodoApi());
 
   async init(): Promise<void> {
-    throw new Error('init: not implemented');
+    await this.service.create('Title1', 'Description1');
+    await this.service.create('Title2', 'Description2');
   }
 
   async add(title: string, description = ''): Promise<void> {
-    throw new Error('add: not implemented');
+    await this.service.create(title, description);
   }
 
   async complete(id: number): Promise<void> {
-    throw new Error('complete: not implemented');
+    const todos = await this.service.search('');
+    const todo = todos.find((t) => t.id === id);
+
+    if (todo && todo.status !== 'COMPLETED') {
+      await this.service.toggleStatus(id);
+
+      if (todo.status === 'PENDING') {
+        await this.service.toggleStatus(id);
+      }
+    }
   }
 
   async list(): Promise<Todo[]> {
-    throw new Error('list: not implemented');
+    return await this.service.search('');
   }
 }
